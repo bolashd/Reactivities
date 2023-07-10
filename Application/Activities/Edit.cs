16d3800c7c690e895b9ce768b5ab0a79617bbdb9
Dane Bolash <dane.bolash@bsdiweb.com>
2023-07-10 09:28:00 -0400

    End of lesson 51

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -20,8 +20,8 @@ namespace Application.Activities
             {
                 _mapper = mapper;
                 _context = context;
-
             }
+            
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
