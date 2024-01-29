7494beeaa16efcd0aacc54d0240db9adda58fd24
Dane Bolash <dane.bolash@bsdiweb.com>
2024-01-29 09:21:49 -0500

    End of lesson 149

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,15 +1,18 @@
 import CommonStore from './commonStore';
 import ActivityStore from './activityStore';
 import { createContext, useContext } from 'react';
+import UserStore from './userstore';
 
 interface Store {
     activityStore: ActivityStore;
     commonStore: CommonStore;
+    userStore: UserStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
     commonStore: new CommonStore(),
+    userStore: new UserStore(),
 };
 
 export const StoreContext = createContext(store);
